4dfafc20a0333f3bd457cc6e2e3dcb91f63b2689
RT-MANO <45135298+RT-MANO@users.noreply.github.com>
2018-11-18 11:20:12 +0800

    Updated from repo

--- src/com/mano/Main.java
+++ src/com/mano/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hai Mano! -" + args[0]);
         System.out.println("Hai Mano 2!");
+        System.out.println("Hai Mano 3!");
     }
 }
