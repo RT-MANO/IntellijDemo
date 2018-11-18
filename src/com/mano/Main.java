878c281858ed9d906db92a032349d40c70768ef5
mano <rt_mano@yahoo.com>
2018-11-18 11:17:56 +0800

    second commit

--- src/com/mano/Main.java
+++ src/com/mano/Main.java
@@ -3,6 +3,7 @@ package com.mano;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hai Mano! -" + args[0]);
+        System.out.println("Hai Mano! -" + args[0]);
+        System.out.println("Hai Mano 2!");
     }
 }
